56da2ed4e853c91a9162a5108982d498946b2090
panlei <panlei106@gmail.com>
2013-08-03 19:02:55 +0800

    add WelcomeActivity

---
+++ src/org/nupter/nupter/activity/WelcomeActivity.java
@@ -0,0 +1,40 @@
+package org.nupter.nupter.activity;
+
+
+
+import org.nupter.nupter.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * 启动画面，闪屏效果
+ * @author panlei
+ *
+ */
+
+public class WelcomeActivity extends Activity{
+    
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN ,
+                WindowManager.LayoutParams. FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_welcome);
+        
+        new Handler().postDelayed(new Runnable() {
+            
+        public void run() {
+            Intent intent = new Intent(WelcomeActivity.this,BaiduMapActivity.class);  
+            startActivity(intent);
+            WelcomeActivity.this.finish();   
+            }
+            }, 1000);    
+    }
+
+
+}
