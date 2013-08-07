cbe2d1b522d5a4b5797a93cff5d7b819b4d4c584
Linxiangyu <lxyweb@gmail.com>
2013-08-07 11:16:20 +0800

    Modify LostAndFoundActivity to FragmentActivity

--- src/org/nupter/nupter/activity/LostAndFoundActivity.java
+++ src/org/nupter/nupter/activity/LostAndFoundActivity.java
@@ -1,17 +1,17 @@
 package org.nupter.nupter.activity;
 
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.PagerTabStrip;
-import android.support.v4.view.PagerTitleStrip;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.TextView;
 import org.nupter.nupter.R;
 
 import java.util.ArrayList;
@@ -23,85 +23,88 @@ import java.util.List;
  * @author <a href="mailto:lxyweb@gmail.com">Lin xiangyu</a>
  *
  */
-public class LostAndFoundActivity extends Activity {
+public class LostAndFoundActivity extends FragmentActivity {
 
-    ViewPager mViewPager;
-    View view1, view2, view3;
-    PagerTitleStrip pagerTitleStrip;
-    PagerTabStrip pagerTabStrip;
-    List<View> viewList;
-    List<String> titleList;
-    Button button;
-    Intent intent;
+   List<Fragment> fragmentList = new ArrayList<Fragment>();
+   List<String> titleList = new ArrayList<String>();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lost_and_found);
-        initView();
-    }
 
-    public void initView() {
-        mViewPager = (ViewPager) findViewById(R.id.viewpager);
-        pagerTabStrip = (PagerTabStrip) findViewById(R.id.pagertab);
-        //pagerTitleStrip = (PagerTitleStrip)findViewById(R.id.pagertitle);
-        pagerTabStrip.setDrawFullUnderline(false);
-        pagerTabStrip.setTextSpacing(50);
-        //  pagerTabStrip.setTabIndicatorColor(getResources().getColor(R.color.gold));
-        //  pagerTabStrip.setBackgroundColor(getResources().getColor(R.color.azure));
-
-
-        view1 = findViewById(R.layout.view_lost);
-        view2 = findViewById(R.layout.view_found);
-
-        LayoutInflater layoutInflater = getLayoutInflater().from(this);
-        view1 = layoutInflater.inflate(R.layout.view_lost, null);
-        view2 = layoutInflater.inflate(R.layout.view_found, null);
-
-        viewList = new ArrayList<View>();
-        viewList.add(view1);
-        viewList.add(view2);
-
-        titleList = new ArrayList<String>();
-        titleList.add("Lost");
-        titleList.add("found");
-
-        PagerAdapter pagerAdapter = new PagerAdapter() {
-            @Override
-            public int getCount() {
-                return viewList.size();
-            }
-
-            @Override
-            public boolean isViewFromObject(View view, Object o) {
-                return view == o;
-            }
-
-            @Override
-            public void destroyItem(ViewGroup container, int position, Object object) {
-                container.removeView(viewList.get(position));
-            }
-
-            @Override
-            public int getItemPosition(Object object) {
-                return super.getItemPosition(object);
-            }
-
-            @Override
-            public CharSequence getPageTitle(int position) {
-                return titleList.get(position);
-            }
-
-            @Override
-            public Object instantiateItem(ViewGroup container, int position) {
-                container.addView(viewList.get(position));
-                return viewList.get(position);
-            }
-        };
-
-        mViewPager.setAdapter(pagerAdapter);
-
+        ViewPager vp = (ViewPager)findViewById(R.id.viewPager);
+        fragmentList.add(new ViewPagerFragment("Page1"));
+        fragmentList.add(new ViewPagerFragment("Page2"));
+        titleList.add("title 1");
+        titleList.add("title 2");
+        vp.setAdapter(new MyPagerAdapter(getSupportFragmentManager(), fragmentList, titleList));
 
     }
+
+
+    class MyPagerAdapter extends FragmentPagerAdapter {
+
+        private List<Fragment> fragmentList;
+        private List<String>   titleList;
+
+        public MyPagerAdapter(FragmentManager fm, List<Fragment> fragmentList, List<String> titleList){
+            super(fm);
+            this.fragmentList = fragmentList;
+            this.titleList = titleList;
+        }
+
+        /**
+         * 得到每个页面
+         */
+        @Override
+        public Fragment getItem(int arg0) {
+            return (fragmentList == null || fragmentList.size() == 0) ? null : fragmentList.get(arg0);
+        }
+
+        /**
+         * 每个页面的title
+         */
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return (titleList.size() > position) ? titleList.get(position) : "";
+        }
+
+        /**
+         * 页面的总个数
+         */
+        @Override
+        public int getCount() {
+            return fragmentList == null ? 0 : fragmentList.size();
+        }
+    }
+
+
+
+
+
+    public class ViewPagerFragment extends Fragment {
+
+        private String   text;
+        private TextView tv = null;
+
+        public ViewPagerFragment(String text){
+            super();
+            this.text = text;
+        }
+
+        /**
+         * 覆盖此函数，先通过inflater inflate函数得到view最后返回
+         */
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.view_pager_fragment, container, false);
+            tv = (TextView)v.findViewById(R.id.textView);
+            tv.setText(text);
+            return v;
+        }
+    }
+
+
 }
