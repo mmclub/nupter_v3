4b108777a757c0797e14d7a9874c332bb57a70be
Linxiangyu <lxyweb@gmail.com>
2013-08-09 14:29:24 +0800

    Change NetworkUtils filename

--- src/org/nupter/nupter/utils/NetUtils.java
+++ src/org/nupter/nupter/utils/NetUtils.java
@@ -12,7 +12,7 @@ import org.nupter.nupter.MyApplication;
  * @author <a href="mailto:lxyweb@gmail.com、
  * ">Lin xiangyu</a>
  */
-public class NetworkUtils {
+public class NetUtils {
 
     public static boolean isNewworkConnected() {
 
