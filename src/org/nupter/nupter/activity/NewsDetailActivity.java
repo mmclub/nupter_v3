a36339213d9aba3ed272ed60bde2474ac07874f0
panlei <panlei106@gmail.com>
2013-08-10 11:27:56 +0800

    some changes

--- src/org/nupter/nupter/activity/NewsDetailActivity.java
+++ src/org/nupter/nupter/activity/NewsDetailActivity.java
@@ -2,11 +2,15 @@ package org.nupter.nupter.activity;
 
 import org.nupter.nupter.R;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
+@SuppressLint("NewApi")
 public class NewsDetailActivity extends Activity{
     
     private TextView contentTV;
@@ -21,7 +25,20 @@ public class NewsDetailActivity extends Activity{
         getContent =  contentIntent.getStringExtra("content");
         contentTV = (TextView)findViewById(R.id.contentTV);
         contentTV.setText(getContent);
-        
+        getActionBar().setDisplayHomeAsUpEnabled(true);
     }
+       @Override
+       public boolean onMenuItemSelected(int featureId, MenuItem item) {
+           switch (item.getItemId()) {
+           case android.R.id.home:
+               onBackPressed();
+               break;
+           default:
+               Toast.makeText(this, "update", Toast.LENGTH_SHORT).show();
+
+               break;
+           }
+           return super.onMenuItemSelected(featureId, item);
+       }
 
 }
